9ae68aee329713e3148d0e8e5c2b57e2dd68d464
Martin Murphy <martin@memqor.com>
2025-02-25 12:07:21 -0800

    update

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -41,10 +41,10 @@ const tempProduct = fs.readFileSync(
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
@@ -53,11 +53,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     // serve the data without re-reading the file
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
