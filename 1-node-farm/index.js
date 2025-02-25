45df3a674c9d05ff30e0a79775266546bb7c782e
Martin Murphy <martin@memqor.com>
2025-02-24 16:41:15 -0800

    15:  HTML Template: Filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,32 +2,6 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-// Blocking, synchronous way
-// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./starter/txt/output.txt", textOut);
-// console.log("File written");
-
-// Non-blocking, asynchronous way
-// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./starter/txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile(
-//         "./starter/txt/final.txt",
-//         `${data2}\n${data3}`,
-//         "utf-8",
-//         (err) => {
-//           console.log("Your file has been written 😘 ");
-//         }
-//       );
-//     });
-//   });
-// });
-// console.log("Will read file!");
-
 ////////////////////////////
 // SERVER
 // only have to read the data once
@@ -35,19 +9,61 @@ const data = fs.readFileSync(
   `${__dirname}/starter/dev-data/data.json`,
   "utf-8"
 );
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8"
+);
+
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    // API
   } else if (pathName === "/api") {
     // serve the data without re-reading the file
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
-    console.log('serving API data');
+    console.log("serving API data");
+
+    // Not found
   } else {
     // write a header and tell browser what content type to expect
     res.writeHead(404, { "Content-type": "text/html" });
