d6c4099c44f2cd6d4b3025bfad6f2d338e0e7f43
Martin Murphy <martin@memqor.com>
2025-02-27 13:19:59 -0800

    57: refactored routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,30 +5,30 @@ const app = express();
 
 //middleware
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
-
-/* app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
- */
+const basePath = '/api/v1/tours';
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+
+  const tour = tours.find((el) => el.id === parseInt(id));
+  console.log(tour);
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+const newTour = (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -47,7 +47,57 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  const { id } = req.params;
+  if (parseInt(id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour here>',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  const { id } = req.params;
+  if (parseInt(id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// // All tours
+// app.get('/api/v1/tours', getAllTours);
+
+// // A Single tour
+// app.get('/api/v1/tours/:id', getTour);
+
+// app.post('/api/v1/tours', newTour);
+
+// // Update a tour
+// //
+// app.patch('/api/v1/tours/:id', updateTour);
+
+// //Delete
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+
+//Refactored routes
+app.route('/api/v1/tours').get(getAllTours).patch(updateTour);
+app.route('/api/v1/tours/:id').get(getTour).post(newTour).delete(deleteTour);
 
 // start up a server
 const port = 3000;
