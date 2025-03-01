5e7c2305e508e14373d4f470e4a4c33614a09553
Martin Murphy <martin@memqor.com>
2025-03-01 15:36:23 -0800

    66: Static files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,6 +9,7 @@ const app = express();
 //middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 😁');
@@ -25,4 +26,4 @@ app.use((req, reqs, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports =app;
+module.exports = app;
